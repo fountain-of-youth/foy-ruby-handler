c6c9080e9fe9a6eef94578891ba21440d6cc08f9
Roberto Soares <roberto.tech@gmail.com>
2013-03-04 19:06:35 -0300

    gem latest version

--- lib/foy_ruby_parser.rb
+++ lib/foy_ruby_parser.rb
@@ -8,6 +8,10 @@ module Foy
         def parse(file)
           Bundler::LockfileParser.new(File.open(file, 'r').read)
         end
+
+        def latest_version_for(gem)
+          Gem.latest_version_for(gem).version
+        end
       end
     end
   end
